19feae39263f00bea0d248ffd1d9cf96910178fe
Cyber Sammy <kailas11252001@gmail.com>
2026-07-14 19:09:24 +0300

    another refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css'
-import ElementsList from './components/ElemetsList'
+import ElementsList from './components/IssuesList'
 import Header from './components/Header'
 import InfoBlock from './components/InfoBlock'
+import type { IssueItem } from './models/issue_item'
+import { IssuePriorities } from './enums/issue_priorities'
+import { IssueStatuses } from './enums/issue_statuses'
+
+const issuesList : IssueItem[] = [
+    {id : 0, title : "Task1", description : "This is task 1", status : IssueStatuses.Planning, priority : IssuePriorities.Low, assignee : "User1"},
+    {id : 1, title : "Task2", description : "This is task 2", status : IssueStatuses.Closed, priority : IssuePriorities.Medium, assignee : "User2"},
+    {id : 2, title : "Task3", description : "This is task 3", status : IssueStatuses.ReadyToDo, priority : IssuePriorities.Medium, assignee : "User1"},
+    {id : 3, title : "Task4", description : "This is task 4", status : IssueStatuses.Done, priority : IssuePriorities.Medium, assignee : "User1"},
+    {id : 4, title : "Task5", description : "This is task 5", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User1"},
+    {id : 5, title : "Task6", description : "This is task 6", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User3"},
+    {id : 6, title : "Task7", description : "This is task 7", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User1"},
+]
+
 
 function App() {
   return (
@@ -12,7 +26,7 @@ function App() {
         <InfoBlock userName='CyberSammy' currentDate='6-7-6767' usersCount={123} issuesForUser={10}></InfoBlock>
       </div>
       <div>
-        <ElementsList/>
+        <ElementsList issues={issuesList} />
       </div>
     </div>
   )
--- frontend/src/components/IssuesList.tsx
+++ frontend/src/components/IssuesList.tsx
@@ -2,30 +2,21 @@ import IssueCard from "./IssueCard";
 import NoElements from "./NoElements";
 import type { IssueItem } from "../models/issue_item";
 
-import { IssueStatuses } from "../enums/issue_statuses";
-import { IssuePriorities } from "../enums/issue_priorities";
-
 import './styles/IssuesList.css'
 
-const issuesList : IssueItem[] = [
-    {id : 0, title : "Task1", description : "This is task 1", status : IssueStatuses.Planning, priority : IssuePriorities.Low, assignee : "User1"},
-    {id : 1, title : "Task2", description : "This is task 2", status : IssueStatuses.Closed, priority : IssuePriorities.Medium, assignee : "User2"},
-    {id : 2, title : "Task3", description : "This is task 3", status : IssueStatuses.ReadyToDo, priority : IssuePriorities.Medium, assignee : "User1"},
-    {id : 3, title : "Task4", description : "This is task 4", status : IssueStatuses.Done, priority : IssuePriorities.Medium, assignee : "User1"},
-    {id : 4, title : "Task5", description : "This is task 5", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User1"},
-    {id : 5, title : "Task6", description : "This is task 6", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User3"},
-    {id : 6, title : "Task7", description : "This is task 7", status : IssueStatuses.InProgress, priority : IssuePriorities.High, assignee : "User1"},
-]
+interface IssuesListProps {
+    issues: IssueItem[];
+}
 
-function ElementsList() {
+function ElementsList({ issues }: IssuesListProps) {
 
-    if (issuesList.length === 0) {
+    if (issues.length === 0) {
         return <NoElements />;
     }
 
     return (
         <div className="elements-list">
-            {issuesList.map(issue => (
+            {issues.map(issue => (
                 <IssueCard key={issue.id} issue={issue} />
             ))}
         </div>
